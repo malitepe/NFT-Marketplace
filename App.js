c5b6f3124b902295ab865368d64d71ef0f8b123d
Muhammed Ali Tepe <51605221+malitepe@users.noreply.github.com>
2022-11-16 22:13:56 +0300

    nft marketplace first commit

--- App.js
+++ App.js
@@ -1,14 +1,45 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import {createStackNavigator, StackView} from "@react-navigation/stack";
+import {NavigationContainer, DefaultTheme} from '@react-navigation/native';
+import {useFonts} from 'expo-font';
 
-export default function App() {
+import Home from './screens/Home';
+import Details from './screens/Details';
+
+const Stack = createStackNavigator();
+
+const theme = {
+  ...DefaultTheme,
+  colors:{
+    ...DefaultTheme.colors,
+    background: "transparent"
+
+  }
+}
+
+const App = () => {
+  const [loaded] = useFonts({
+    InterBold : require('./assets/fonts/Inter-Bold.ttf'),
+    InterSemiBold : require("./assets/fonts/Inter-SemiBold.ttf"),
+    InterMedium : require("./assets/fonts/Inter-Medium.ttf"),
+    InterRegular : require("./assets/fonts/Inter-Regular.ttf"),
+    InterLight : require("./assets/fonts/Inter-Light.ttf"),
+  });
+
+  if(!loaded) return null;
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer theme={theme}>
+      <Stack.Navigator screenOptions={{headerShown: false}}
+      initialRouteName="Home"      >
+        <Stack.Screen name="Home" component={Home}/>
+        <Stack.Screen name="Details" component={Details}/>
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
+
+export default App;
 
 const styles = StyleSheet.create({
   container: {
